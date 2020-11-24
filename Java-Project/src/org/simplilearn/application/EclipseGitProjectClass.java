605bc07793d5ff526758712a85179b37fd4363c1
Nick Baynham <nick.baynham@oracle.com>
2020-11-24 17:47:55 -0500

    First commit to the Eclipse Git Project

---
+++ Java-Project/src/org/simplilearn/application/EclipseGitProjectClass.java
@@ -0,0 +1,8 @@
+package org.simplilearn.application;
+
+public class EclipseGitProjectClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is an Eclipse Project");
+	}
+}
---
+++ Java-Project/src/org/simplilearn/application/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author nickbaynham
+ *
+ */
+package org.simplilearn.application;
